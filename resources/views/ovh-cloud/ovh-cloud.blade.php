9e39d888e97816087609993983b63637011b1b57
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2021-03-03 09:05:12 +0500

    added detail of vrack and fix links

--- resources/views/ovh-cloud/ovh-cloud.blade.php
+++ resources/views/ovh-cloud/ovh-cloud.blade.php
@@ -12,10 +12,10 @@
                         <div class="col-sm-12">
                             <nav aria-label="breadcrumb" class="text-center iq-breadcrumb-two">
                                 <h2 class="title">
-                                    Floating IPs: Failover IPs, Geo IPs & IP blocks                     </h2>
+                                    OVH CLOUD                   </h2>
                                 <ol class="breadcrumb main-bg">
-                                    <li class="breadcrumb-item"><a href="javascript:void(0)"><i class="fa fa-home mr-2"></i>Bare Metal</a></li>
-                                    <li class="breadcrumb-item active"> Ip</li>
+                                    <li class="breadcrumb-item"><a href="javascript:void(0)"><i class="fa fa-home mr-2"></i>Network and security</a></li>
+                                    <li class="breadcrumb-item active"> OVH CLOUD</li>
                                 </ol>
                             </nav>
                         </div>
@@ -38,20 +38,15 @@
                     <div class="iq-title-box iq-title-default iq-title-box-2">
                         <div class="iq-title-icon">
                         </div>
-                        <span class="iq-subtitle">IP</span>
-                        <h2 class="iq-title">Floating IPs for added flexibility to develop your applications</h2>
-                        <p class="iq-title-desc">DarkCloud dedicated servers, VPS and Public Cloud instances include an IPv4 (/32) pre-configured and IPv6 IP block (/64).
+                        {{-- <span class="iq-subtitle">IP</span> --}}
+                        <h2 class="iq-title">Expand your reach with OVHcloud datacentres</h2>
+                        <p class="iq-title-desc">With our OVHcloud Connect hybrid connection solution, you can form a secure, high-performance link between your company network and the OVHcloud vRack.
+<br>
+                            Data is transferred through our infrastructures via our Direct solution, and you get a dedicated connection with 1Gbit/s or 10Gbit/s bandwidth.
+                            <br>
+                            You can also choose our Provider solution, with one of our partners (Equinix, Megaport and more to come). With this solution, you get from 200Mbit/s to 5Gbit/s bandwidth, and even more global coverage.</p>
 
-                            For outstanding flexibility, users can attach up to 256 additional IPs.
                             
-                            What is a floating IP? A floating IP is an additional IP that can be switched from one service to another in the same datacentre. You can also switch IPs between different datacentres, if the datacentres are located in the same country. </p>
-
-                            <p>The key benefits of Floating IPs are:</p>
-                            <ul>
-                                <li>enabling IP blocks, so you can configure multiple IP addresses to your services</li>
-                                <li>allowing for failover, so you can switch between services effortlessly</li>
-                                <li>assigning additional geolocated IPs to expand your global audiences</li>
-                            </ul>
                     </div>
                     
                     </div>
@@ -72,9 +67,9 @@
                                 <img src="{{URL::asset('images/icon-box/01.png')}}" class="img-fluid" alt="QLOUD">
                             </div>
                             <div class="icon-box-content">
-                                <h5 class="icon-box-title"> <a>IP blocks</a>
+                                <h5 class="icon-box-title"> <a>Hybrid and multi-cloud</a>
           </h5>
-                                <p class="icon-box-desc">By adding IP blocks, you can configure multiple IP addresses to your services. You can assign IPs into groups of 4, 8, 16, or up to 256 per server, with unlimited block publications. Via the API, blocks can be separated so that you can manage your applications easily. This makes upgrades simple, as you can reroute your IP to new hardware, rather than having to reconfigure the static IP. With multiple IPs grouped into blocks, you can manage your platforms more efficiently — upgrading and migrating from one service to another with ease. IP blocks increase your control and speed, so that you can execute your business strategies and responses quickly.</p>
+                                <p class="icon-box-desc">A connection between your network and OVHcloud infrastructures, compatible with all of our cloud solutions. You get between 200Mbit/s and 10Gbit/s guaranteed bandwidth, and minimal latency on the public network (internet).</p>
                             </div>
                         </div>
                     </div>
@@ -84,9 +79,11 @@
                                 <img src="{{URL::asset('images/icon-box/02.png')}}" class="img-fluid" alt="QLOUD">
                             </div>
                             <div class="icon-box-content">
-                                <h5 class="icon-box-title"> <a href="javascript:void(0)">Failover Capabilities</a>
+                                <h5 class="icon-box-title"> <a href="javascript:void(0)">Secure</a>
           </h5>
-                                <p class="icon-box-desc"> Failover IPs, also named floating IPs, can be switched from one service to another in a few seconds. This means you get an uninterrupted hosting solution, and it helps you overcome issues such as hardware failure, system overruns, and other kinds of infrastructure issues. This way, if you switch from one service to another, you can keep the entire configuration linked to that IP. Floating IPs provide a solution which removes interruption or downtime if servers are overrun, and can be used in this way for “Always On” availability.</p>
+                                <p class="icon-box-desc">We implement the latest security policies to ensure your data and applications are available and secure. 
+
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -95,9 +92,9 @@
                             <div class="icon-box-img">
                                 <img src="{{URL::asset('images/icon-box/03.png')}}" class="img-fluid" alt="QLOUD"> </div>
                             <div class="icon-box-content">
-                                <h5 class="icon-box-title"> <a href="javascript:void(0)">IP Geolocation </a>
+                                <h5 class="icon-box-title"> <a href="javascript:void(0)">Simple and fast </a>
           </h5>
-                                <p class="icon-box-desc">With our announcement IP service, you can expand your project for a global audience. With DarkCloud, you can link to additional geolocated IPs. Additional IPs can be geolocated in 14 available destinations. This considerably improves the ranking of your projects in popular search engines, radically improving your webpage SEO status.    </p>
+                                <p class="icon-box-desc">Automated deployment, which is easy to use and even quicker — especially with our partners. You can improve the quality of your services by configuring your hybrid solution directly via the OVHcloud Control Panel.    </p>
                             </div>
                         </div>
                     </div>
@@ -110,36 +107,10 @@
         <!-- Icon-box End -->
 
 
-        <section class="blue-bg iq-help">
-            <div class="container">
-            <div class="row">
-               <div class="col-lg-12">
-                  <div class=" text-left iq-title-box mb-0 iq-title-white iq-title-box-2">
-                  <div class="iq-title-icon">
-                              </div>
-                  
-                  <h2 class="iq-title text-capitalize">
-                    To purchase additional IP addresses and make the most of floating IPs, go to DarkCloud control panel   </h2>
-                </div>
-            </div>
-            <div class="col-lg-12 text-center align-self-center">
-                <div class="iq-btn-container d-inline-block mr-4">
-                      <p class="text-light mt-4"> Free Monthly Subscription</p>
-                 <a class="iq-button iq-hover d-inline" href="#">  
-                    Control Panel     
-                </a>
-            </div>
-            
-            </div>
-            </div>
-            </div>
-            </section>
-
-
              <!-- Main-Content Start -->
       <div class="main-content">
           <div class="container">
-              <h2 class="p-2">Floating IPs to meet your needs</h2>
+              <h2 class="p-2">Explore OVHcloud Connect in detail</h2>
           </div>
         <section >
            <img src="{{URL::asset('images/others/shape1.png')}}" class="img-fluid shape-right" alt="QLOUD">
@@ -153,10 +124,8 @@
                        <div class="iq-title-icon">
                        </div>
                        <h2 class="iq-title text-capitalize">
-                        Create complex infrastructures without single points of failure </h2>
-                       <p class="iq-title-desc pr-lg-5">Developing complex infrastructures that are resilient to issues is no easy feat. It requires hardware to be networked in a way that can respond to breakdowns, service overload, and maintenance issues.
-
-                        With floating IPs, also known as failover IPs, you can create complex infrastructures that are able to withstand a wide variety of issues. For example, you can switch your existing infrastructure to a load balancer without disrupting or interrupting your customer services. Workloads can be balanced autonomously, while your online space continues as normal. There will be no noticeable IP address changes as you will always use floating IP, which means no DNS propagation delay.</p></div>
+                        Global coverage through points of presence from both OVHcloud and our partners </h2>
+                       <p class="iq-title-desc pr-lg-5">With our extended network, we are as close as possible to your datacentres. Connect your network to OVHcloud infrastructures via our global points of presence, or those offered by our partners.</p></div>
                        
                     </div>
                  </div>
@@ -174,10 +143,10 @@
                          <div class="iq-title-icon">
                          </div>
                          <h2 class="iq-title text-capitalize">
-                            Host multiple websites on the same instance </h2>
-                         <p class="iq-title-desc pr-lg-5">For most online projects, hosting more than one website is a business essential. For any kind of dynamic web page — such as a blog, online store, or membership portal — it is likely that you will need an admin page as well as a public-facing website to deal with ongoing changes. Or if you support multiple languages, you will need multiple webpages. Floating IPs allow you to keep your websites on the server, but without the risk of excessive downtime or website crashes due to overloads. 
+                            Layer 2 or layer 3 connection</h2>
+                         <p class="iq-title-desc pr-lg-5">A layer 2 connection simplifies the way you connect your datacentre’s campus network to your OVHcloud vRack private network.
 
-                            You can host multiple websites on a primary server by using two floating IPs. Some of your websites can be hosted on one floating IP, and the rest on another. When one server isn’t powerful enough to host the website for both the floating IPs, you can get a new server, then switch the routing of one floating IP to the new server. This relieves the primary server of the heavy workload, so it can be operational again.</p></div>
+                            With a layer 3 connection, you can connect your company’s WAN network to your OVHcloud vRack private network, so that it is considered as a site within your company network. This option is only available for our Provider solution.</p></div>
                          
                       </div>
                       <div class="col-lg-6 col-sm-12 mb-lg-0 mb-4 wow fadeInLeft">
@@ -202,10 +171,10 @@
                          <div class="iq-title-icon">
                          </div>
                          <h2 class="iq-title text-capitalize">
-                            Grow your business </h2>
-                         <p class="iq-title-desc pr-lg-5">Growing your storage, expanding clusters and other changes that promote business growth come with a risk of downtime, which can cost your business dearly in sales and reputation. With floating IPs, you can curb the risk of downtime altogether. By following the steps to integrate a failover IP system for your projects, you can expand your infrastructure with uninterrupted performance on each of your websites
+                            Geographical redundancy and SLA</h2>
+                         <p class="iq-title-desc pr-lg-5">Layer 3 networks require one or more private external BGP (Border Gateway Protocol) sessions to be established between your company and OVHcloud.
 
-                            Importantly, floating IPs are highly customisable, and therefore can be used to solve business goals. For example, with IP Announcement services, you can announce multiple geolocations for your IP addresses. This is incredibly important for SEO optimisation, as it considerably improves the ranking of your projects in the most popular search engines.</p></div>
+                            For critical environments, redundancy is strongly recommended with two OVHcloud Connect solutions distributed between two geographically distinct points of presence, using BGP redundancy mechanisms.</p></div>
                          
                       </div>
                    </div>
@@ -214,19 +183,8 @@
   
              <section>
                  <div class="container">
-                     <h2>What is a failover IP/RIPE IP?</h2>
-                     <h4>What is a failover IP?</h4>
-                     <p>A failover IP, also called a floating IP, enhances service continuity (“failover”) for your applications and systems. With failover IP technology, you can switch the IP addresses from one service to another in less than a minute, with almost no service interruption for your users. This mechanism can be used during service migrations, moving projects from development to production environments, and switching services to a backup server in the event of an incident.</p>
-                     <h4>How do I get an IP address?</h4>
-                     <p>To use both a failover IP setup and RIPE IP, you will need to know how to get an IP address. With DarkCloud, getting an IP address is simple — log in to the DarkCloud Control Panel, and follow the appropriate steps. With a dedicated server, IPs can be configured in alias or bridge mode for virtualisation purposes.</p>
-                     <h4>What is a RIPE IP address?</h4>
-                    <p>A RIPE IP address is a European IP address managed and distributed by European IP Networks (Réseaux IP Européens in French). It is an open community that ensures administrative and technical conditions to maintain and develop the Internet. One of the main activities of RIPE is to allocate the IP blocks to ISPs (Internet Service Providers) and register the data in a public database, including information such as a country’s IP block ranges, current IP allocation and IP geolocation.
-                    </p>
-                    <p>
-                        At DarkCloud you can choose the geolocation of your RIPE IP block, and choose between 14 European countries: France, Ireland, Portugal, the United Kingdom, Italy, Switzerland, Spain, Poland, Czech Republic, the Netherlands, Finland, Lithuania, Germany and Belgium. With RIPE IP blocks, you can tailor your addressing plan to fit your needs, choosing blocks from 4 to 256 IPs and managing their reverse DNS (PTR) in the DarkCloud Control Panel.
-                    </p>
-                    <p>
-                        For IPs geolocated in Canada or the United States, ARIN – the American Registry for Internet Numbers – oversees the management and distribution of IP addresses. ARIN IP addresses can be used in the same way as other RIPE IPs, and can be assigned to IP blocks or used as failover IPs.</p>
+                     <h2>The advantage of BGP</h2>
+                     <p>Border Gateway Protocol (BGP) is an autonomous inter-system routing protocol that replaced the External Gateway Protocol (EGP) as a routing exchange protocol. It enables data to be transferred between autonomous systems used for OVHcloud Connect. Compared to internal routing protocols (e.g. RIP or IGRP), it does not take into account the distance between a router and a destination — instead, it takes a more precise set of parameters that can be defined. Its main advantage is its ability to handle a large number of routes that can be selected according to a specific policy. For example, you may choose to limit the number of connected autonomous systems, refuse transit, or prioritise some systems over others. This means it provides protection against loops for autonomous systems. To a lesser extent, it also enables you to manage the traffic load depending on the network announcement.</p>
 
                  </div>
              </section>
